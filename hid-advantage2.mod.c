be4c8478756fe2b4e187a08e1b4bc8dd685e731f
Nikolay Yakimov <root@livid.pp.ru>
2022-01-03 04:46:25 +0300

    commit

--- hid-advantage2.mod.c
+++ hid-advantage2.mod.c
@@ -1,10 +1,12 @@
 #include <linux/module.h>
 #define INCLUDE_VERMAGIC
 #include <linux/build-salt.h>
+#include <linux/elfnote-lto.h>
 #include <linux/vermagic.h>
 #include <linux/compiler.h>
 
 BUILD_SALT;
+BUILD_LTO_INFO;
 
 MODULE_INFO(vermagic, VERMAGIC_STRING);
 MODULE_INFO(name, KBUILD_MODNAME);
